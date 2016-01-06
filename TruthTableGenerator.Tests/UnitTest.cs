10eb43f7769f47b83938b593bc77e3a12b5c2031
Skopher <jenks333@gmail.com>
2016-01-05 21:24:02 -0800

    Added Tests project.

---
+++ TruthTableGenerator.Tests/UnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace TruthTableGenerator.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
