d63ca3fa56a3fcd41a12086a9043d95408d51ad0
Skopher <jenks333@gmail.com>
2016-01-07 19:11:47 -0800

    Aha! I created a UWA Test project, not a regular Unit Test Project...NUnit, TestAdapter, and Moq installed correctly. Wrote stub test, and VS recognized it.

---
+++ TruthTableGenerator.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+using Moq;
+
+namespace TruthTableGenerator.Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
