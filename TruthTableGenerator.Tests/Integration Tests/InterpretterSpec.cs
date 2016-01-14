1b03d3556835f99fdb23e1a15837680abd41616c
Skopher <jenks333@gmail.com>
2016-01-13 22:12:10 -0800

    Added Interpretter spec integration test and non-compiling test. Not yet sure of all the components; will add Unit Tests as they are learned. An open question: How to test the syntax of a language? Exhaustively?

---
+++ TruthTableGenerator.Tests/Integration Tests/InterpretterSpec.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Moq;
+
+/*
+Not sure how I will put up rail diagrams for the syntax...I'll probably upload them 
+separately as handwritten images/pdfs.
+
+For now, the symbols are:
+
+Symbol    | Meaning
+  !       | NOT
+  &       | AND
+  |       | OR (capital or lowercase)
+  @       | XOR 
+
+
+*/
+
+namespace TruthTableGenerator.Tests.Integration_Tests
+{
+    [TestFixture]
+    class InterpretterSpec
+    {
+        [TestCase("1&1", ExpectedResult = true)]
+        [TestCase("1&0", ExpectedResult = false)]
+        [TestCase("0&1", ExpectedResult = false)]
+        [TestCase("0&0", ExpectedResult = false)]
+        public bool GivenANDexpressionsThenInterpretterShouldReturnCorrectANDInterpretation(string ANDexpression)
+        {
+            ITruthInterpretter interpretter = new TruthInterpretter();
+
+            bool result = interpretter.interpret(ANDexpression);
+
+            return result;
+        }
+
+    }
+}
